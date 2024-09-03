16ecc97ed4ffae31da94551c401f4255ac76746c
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-09-03 23:13:13 +0300

    Anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_of_words):
+        anagrams = list()
+        for item in list_of_words:
+            if sorted(item) == sorted(self.word):
+                anagrams.append(item)
+        return anagrams
+
+word = Anagram('word')
+print(word.match(['hello', 'goodbye'])) 
+word2 = Anagram('enlist')
+print(word2.match(['listen', 'silent', 'hippopotamus']))
